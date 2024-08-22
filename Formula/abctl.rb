02dc43ffe59a0fa2e91355a729dcdbed0f2debfb
Alex Buchanan <alex.buchanan@airbyte.io>
2024-08-22 16:35:29 -0700

    add revision

--- Formula/abctl.rb
+++ Formula/abctl.rb
@@ -2,8 +2,8 @@ class Abctl < Formula
   desc "Abuchanan's test for abctl"
   homepage "https://github.com/abuchanan-airbyte/abctl"
   url "https://github.com/abuchanan-airbyte/abctl.git",
-      tag:      "v0.0.1",
-      revision: "ac0c60ad91df6dacd1a7fbc4d6364efaf9a7b409"
+      tag:      "v0.0.6",
+      revision: "317761b8e152b3c416b7fe90b2332baf6ef599cf"
 
   license "MIT"
 
